9e55d5e3e91a96e582ebf271692e2381172b46c7
CieloBussetti <cielobw@gmail.com>
2019-09-12 10:30:23 -0300

    primer ejemplo de header

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body id="body">
+    <header id="header">
+        <div class="header">
+            <a class="logo" href="#">
+                <img src="img.jpg" alt="Logo">
+                <h1>Randfood</h1>
+            </a>
+              
+            <nav class="navReg">
+                <ul>
+                    <li>
+                        <p><a href="#">Sign Up</a></p>
+                    </li>
+                    <p>|</p>
+                    <li>
+                        <p><a href="#">Log In</a></p>
+                    </li>
+                </ul> 
+            </nav>
+        </div>
+        <div class="headerBottom">
+           <nav class="navBar">
+                <ul>
+                    <li>
+                        <p><a href="#">Ejemplo</a></p>
+                    </li>
+                    <li>
+                        <p><a href="#">Ejemplo</a></p>
+                    </li>
+                    <li>
+                        <p><a href="#">Ejemplo</a></p>
+                    </li>
+                    <li>
+                        <p><a href="#">Ejemplo</a></p>
+                    </li>
+                </ul>
+            </nav> 
+        </div>
+    </header>
+</body>
+</html>
