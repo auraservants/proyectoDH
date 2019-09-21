464ce6f681325956cdf48586ca65ae6e78934d68
CieloBussetti <cielobw@gmail.com>
2019-09-21 20:18:19 -0300

    arreglo de la home

--- index.php
+++ index.php
@@ -1,159 +1,162 @@
 <?php require_once("head.php"); ?>
 
-<body id="body" class="home">
-	<?php include_once("header.php"); ?> 
-	<main>
-		<section class="fotoDeFondo">
-			<div class="text">
-				<h1>La forma más genial de comer.</h1>
-				<h2>Tán fácil como elegir tus ingredientes y listo!</h2>
-			</div>
-		</section>
 
-		<section class="steps container" id="steps">
-			<h3>Randfood es muy simple</h3>
-			<p class="after">Sigue estos pasos para armar tu plato</p>
-			<div class="card">
-				<div class="content-card">
-					<div class="steps">
-						<img src="image/step1.jpg" alt="">
-					</div>
-					<div class="steps-text">
-						<h4>Elige los ingredientes</h4>
-						<p>De acuerdo a tus gustos.</p>
-					</div>
-				</div>
-				<div class="content-card">
-					<div class="steps">
-						<img src="image/step2.jpg" alt="">
-					</div>
-					<div class="steps-text">
-						<h4>Elige los condimentos</h4>
-						<p>Los que prefieras.</p>
-					</div>
-				</div>
-				<div class="content-card">
-					<div class="steps">
-						<img src="image/step3.jpg" alt="">
-					</div>
-					<div class="steps-text">
-						<h4>Ya tienes tu plato!</h4>
-						<p>Espera para deleitarte.</p>
-					</div>
-				</div>
-			</div>
-		</section>
+<div class="home">
+	<?php include_once("header.php"); ?>
+</div>
+	
+<main>
+	<section class="fotoDeFondo">
+		<div class="text">
+			<h1>La forma más genial de comer.</h1>
+			<h2>Tán fácil como elegir tus ingredientes y listo!</h2>
+		</div>
+	</section>
 
-		<section class="about container" id="about">
-			<h3>Que es Randfood?</h3>
-			<p class="after">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-			<div class="content-about">
-				<div class="about-img">
-					<img src="image/about.jpg" alt="">
+	<section class="steps container" id="steps">
+		<h3>Randfood es muy simple</h3>
+		<p class="after">Sigue estos pasos para armar tu plato</p>
+		<div class="card">
+			<div class="content-card">
+				<div class="steps">
+					<img src="image/step1.jpg" alt="">
 				</div>
-				<div class="about-text">
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aliquam quos hic facere amet, voluptas eum unde natus corrupti culpa consequuntur non temporibus excepturi laborum placeat sunt recusandae doloribus libero.
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae nemo voluptas, id asperiores accusantium, labore consequatur corporis cumque ipsam officia reprehenderit praesentium est obcaecati temporibus architecto magni amet non molestiae.
-					</p>
+				<div class="steps-text">
+					<h4>Elige los ingredientes</h4>
+					<p>De acuerdo a tus gustos.</p>
 				</div>
 			</div>
-		</section>
+			<div class="content-card">
+				<div class="steps">
+					<img src="image/step2.jpg" alt="">
+				</div>
+				<div class="steps-text">
+					<h4>Elige los condimentos</h4>
+					<p>Los que prefieras.</p>
+				</div>
+			</div>
+			<div class="content-card">
+				<div class="steps">
+					<img src="image/step3.jpg" alt="">
+				</div>
+				<div class="steps-text">
+					<h4>Ya tienes tu plato!</h4>
+					<p>Espera para deleitarte.</p>
+				</div>
+			</div>
+		</div>
+	</section>
 
-		<section class="ingredients container" id="products">
-			<h3>Ingredientes destacados</h3>
-			<p class="after">Elige el tuyo!</p>
-			<div id="ingredients">
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/1.jpg" alt="">
-					<h4>Tomato</h4>
-					<p>$5,00</p>
-					<a href="#">Agregar</a>
+	<section class="about container" id="about">
+		<h3>Que es Randfood?</h3>
+		<p class="after">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+		<div class="content-about">
+			<div class="about-img">
+				<img src="image/about.jpg" alt="">
+			</div>
+			<div class="about-text">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aliquam quos hic facere amet, voluptas eum unde natus corrupti culpa consequuntur non temporibus excepturi laborum placeat sunt recusandae doloribus libero.
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae nemo voluptas, id asperiores accusantium, labore consequatur corporis cumque ipsam officia reprehenderit praesentium est obcaecati temporibus architecto magni amet non molestiae.
+				</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="ingredients container" id="products">
+		<h3>Ingredientes destacados</h3>
+		<p class="after">Elige el tuyo!</p>
+		<div id="ingredients">
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/1.jpg" alt="">
+				<h4>Tomato</h4>
+				<p>$5,00</p>
+				<a href="#">Agregar</a>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/2.jpg" alt="">
+				<h4>Salmon</h4>
+				<p>$1,00</p>
+				<div class="btn"><a href="#">Agregar</a></div>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/3.jpg" alt="">
+				<h4>Huevo</h4>
+				<p>$8,00</p>
+				<a href="#">Agregar</a>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/4.jpg" alt="">
+				<h4>Palta</h4>
+				<p>$30,00</p>
+				<a href="#">Agregar</a>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/5.jpg" alt="">
+				<h4>Tomato</h4>
+				<p>$5,00</p>
+				<a href="#">Agregar</a>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/6.jpg" alt="">
+				<h4>Salmon</h4>
+				<p>$1,00</p>
+				<div class="btn"><a href="#">Agregar</a></div>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/3.jpg" alt="">
+				<h4>Huevo</h4>
+				<p>$8,00</p>
+				<a href="#">Agregar</a>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/1.jpg" alt="">
+				<h4>Palta</h4>
+				<p>$30,00</p>
+				<a href="#">Agregar</a>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/2.jpg" alt="">
+				<h4>Palta</h4>
+				<p>$30,00</p>
+				<a href="#">Agregar</a>
+			</div>
+			<div class="card-products" id="ingredient">
+				<img src="image/ingredients/3.jpg" alt="">
+				<h4>Palta</h4>
+				<p>$30,00</p>
+				<a href="#">Agregar</a> 
+			</div>
+		</div>
+	</section>
+		
+	<section class="features container" id="features">
+		<div class="container">
+			<h3>En Randfood te aseguramos</h3>
+			<p class="after">lorem ipsum dolom.</p>
+			<div class="features">
+				<div class="div-features">
+					<img src="image/features1.png" alt="">
+					<h4>Calidad Premium</h4>
+					<p>Lorem ipsum, dolor sit amet consectetil.</p>
 				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/2.jpg" alt="">
-					<h4>Salmon</h4>
-					<p>$1,00</p>
-					<div class="btn"><a href="#">Agregar</a></div>
+				<div class="div-features">
+					<img src="image/features2.png" alt="">
+					<h4>Personal Calificado</h4>
+					<p>Lorem ipsum, dolor sit amet consectetil.</p>
 				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/3.jpg" alt="">
-					<h4>Huevo</h4>
-					<p>$8,00</p>
-					<a href="#">Agregar</a>
+				<div class="div-features">
+					<img src="image/features3.png" alt="">
+					<h4>Rápida Ejecución</h4>
+					<p>Lorem ipsum, dolor sit amet consectetil.</p>
 				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/4.jpg" alt="">
-					<h4>Palta</h4>
-					<p>$30,00</p>
-					<a href="#">Agregar</a>
-				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/5.jpg" alt="">
-					<h4>Tomato</h4>
-					<p>$5,00</p>
-					<a href="#">Agregar</a>
-				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/6.jpg" alt="">
-					<h4>Salmon</h4>
-					<p>$1,00</p>
-					<div class="btn"><a href="#">Agregar</a></div>
-				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/3.jpg" alt="">
-					<h4>Huevo</h4>
-					<p>$8,00</p>
-					<a href="#">Agregar</a>
-				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/1.jpg" alt="">
-					<h4>Palta</h4>
-					<p>$30,00</p>
-					<a href="#">Agregar</a>
-				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/2.jpg" alt="">
-					<h4>Palta</h4>
-					<p>$30,00</p>
-					<a href="#">Agregar</a>
-				</div>
-				<div class="card-products" id="ingredient">
-					<img src="image/ingredients/3.jpg" alt="">
-					<h4>Palta</h4>
-					<p>$30,00</p>
-					<a href="#">Agregar</a> 
+				<div class="div-features">
+					<img src="image/features4.png" alt="">
+					<h4>Devolución Segura</h4>
+					<p>Lorem ipsum, dolor sit amet consectetil.</p>
 				</div>
 			</div>
-		</section>
-			
-		<section class="features container" id="features">
-			<div class="container">
-				<h3>En Randfood te aseguramos</h3>
-				<p class="after">lorem ipsum dolom.</p>
-				<div class="features">
-					<div class="div-features">
-						<img src="image/features1.png" alt="">
-						<h4>Calidad Premium</h4>
-						<p>Lorem ipsum, dolor sit amet consectetil.</p>
-					</div>
-					<div class="div-features">
-						<img src="image/features2.png" alt="">
-						<h4>Personal Calificado</h4>
-						<p>Lorem ipsum, dolor sit amet consectetil.</p>
-					</div>
-					<div class="div-features">
-						<img src="image/features3.png" alt="">
-						<h4>Rápida Ejecución</h4>
-						<p>Lorem ipsum, dolor sit amet consectetil.</p>
-					</div>
-					<div class="div-features">
-						<img src="image/features4.png" alt="">
-						<h4>Devolución Segura</h4>
-						<p>Lorem ipsum, dolor sit amet consectetil.</p>
-					</div>
-				</div>
-			</div>
-		</section>
-	</main>
+		</div>
+	</section>
+</main>
 
 <?php include_once("footer.php"); ?> 
