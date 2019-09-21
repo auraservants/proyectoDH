b0209441b4b4748d0eea50b1b7980ee10d4c2ef1
brfede <bloomerfede@gmail.com>
2019-09-21 18:43:35 -0300

    Se modifica clases contact.php y animaciones de su css

--- contact.php
+++ contact.php
@@ -1,20 +1,20 @@
 <?php include_once("head.php"); ?>
 <?php include_once("header.php"); ?>
-<main id="contact">
-    <h2>Contactanos</h2>
-    <form action="validar.php" method="post">
-        <div>
-            <input class="formContact" type="text" name="name" placeholder="Nombre:">
-        </div>
-        <div>
-            <input class="formContact" type="email" name="email" placeholder="Email:">
-        </div>
-        <div>
-            <textarea class="formContact mensaje" name="mensaje" id="mensaje" cols="30" rows="10" placeholder="Escribe tu mensaje..."></textarea>
-        </div>
-        <div>
-            <input class="bottonContact" type="submit">
-        </div>
-    </form>
+<main class="contact">
+	<h2 class="contact__title">Contactanos</h2>
+	<form action="validar.php" method="post" class="contact__form">
+		<div>
+			<input class="contact__input" type="text" name="name" placeholder="Nombre:">
+		</div>
+		<div>
+			<input class="contact__input" type="email" name="email" placeholder="Email:">
+		</div>
+		<div>
+			<textarea class="contact__input mensaje" name="mensaje" id="mensaje" cols="30" rows="10" placeholder="Escribe tu mensaje..."></textarea>
+		</div>
+		<div>
+			<button class="bottonContact" type="submit">Enviar</button>
+		</div>
+	</form>
 </main>   
 <?php include_once("footer.php"); ?>
--- footer.php
+++ footer.php
@@ -1,28 +1,28 @@
-<footer>
-    <div class="footer container">
-        <div class="logo-footer">
-            <img src="image/randfood.png" alt="logo-footer">
-        </div>
-        <div class="social-icons">
-            <a href="www.google.com"><img src="image/facebook.png" alt=""></a>
-            <a href="www.google.com"><img src="image/instagram.png" alt=""></a>
-            <a href="www.google.com"><img src="image/twitter.png" alt=""></a>
-            <a href="www.google.com"><img src="image/whatsapp.png" alt=""></a>
-        </div>  
-    </div>
+    <footer>
+			<div class="footer container">
+				<div class="logo-footer">
+					<img src="image/randfood.png" alt="logo-footer">
+				</div>
+				<div class="social-icons">
+					<a href="www.google.com"><img src="image/facebook.png" alt=""></a>
+					<a href="www.google.com"><img src="image/instagram.png" alt=""></a>
+					<a href="www.google.com"><img src="image/twitter.png" alt=""></a>
+					<a href="www.google.com"><img src="image/whatsapp.png" alt=""></a>
+				</div>  
+			</div>
 
-    <div class="legal-info">
-        <div class="copyright-text">
-            <p>Copyright © 2019 Randfood. All rights reserved.</p>
-        </div>
-        <div class="payment-logos">
-            <img src="image/payment.png" alt="">
-        </div>
-    </div> 
-</footer>
+			<div class="legal-info">
+				<div class="copyright-text">
+					<p>Copyright © 2019 Randfood. All rights reserved.</p>
+				</div>
+				<div class="payment-logos">
+					<img src="image/payment.png" alt="">
+				</div>
+			</div> 
+    </footer>
 
-<script src="js/jquery.js"></script>
-<script src="js/main.js"></script>
+    <script src="js/jquery.js"></script>
+		<script src="js/main.js"></script>
 
-</body>
+  </body>
 </html>
