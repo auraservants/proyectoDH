f51e20ead4d8c1e6501092296dec349caa0c7304
aura servants <auraservants@gmail.com>
2019-10-15 22:22:52 -0300

    cambios footer y algunas imagenes

--- footer.php
+++ footer.php
@@ -4,6 +4,9 @@
 			<div class="footer-body">
 				<div class="brand-footer">
 					<img src="image/randfood.png" alt="logo-footer">
+					<p>Av. Callao 335, CABA, Argentina.</p>
+					<p>Teléfono: 0800 – 123 456 78</p>
+					<p>Email: info@randfood.com</p>
 					<div class="social-footer">
 					<a href="www.google.com"><img src="image/facebook.png" alt=""></a>
 					<a href="www.google.com"><img src="image/instagram.png" alt=""></a>
@@ -13,7 +16,7 @@
 				</div>
 					
 				<div class="links-footer">
-					<h3 class="Links">Links</h3>
+					<h3 class="Links">Navegación</h3>
 						<ul>
 							<li><a href="index.php" id="link-home" class="btn-header">Inicio</a></li>
                     		<li><a href="products.php" id="link-shop" class="btn-header">Shop</a></li>
@@ -22,13 +25,25 @@
 						</ul>
 				</div>
 
+				<div class="links-footer">
+					<h3 class="Links">Información</h3>
+						<ul>
+							<li><a href="index.php" id="link-home" class="btn-header">Mi cuenta</a></li>
+                    		<li><a href="products.php" id="link-shop" class="btn-header">Carrito</a></li>
+                    		<li><a href="faqs.php" id="link-us" class="btn-header">Ofertas</a></li>
+							<li><a href="contact.php" id="link-contact" class="btn-header">Garantía</a></li>
+						</ul>
+				</div>
+
 				<div class="newsletter-footer">
 					<h3 class="Newsletter">Suscribete al newsletter</h3>
-					
-					<input class="newsletter-items" type="EMAIL" placeholder="Ingresa tu e-mail">
-					<br>
-					<button class="btn-footer" value="SUBMIT NOW">SUSCRIBIRME</button>
-					
+					<p>Enterate primero de todas las novedades!</p>
+					<form action="" method="">
+					<div class="email-box">
+						<input class="text-box" type="email" name="" value="" placeholder="Ingresa tu e-mail">
+						<button class="btn-newsletter" type="button" name="button"><i class="fas fa-paper-plane"></i></button>
+					</div>
+					</form>
 				</div>
 			</div>
 
